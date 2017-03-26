7ef57d870eb39bc17c7f4a41f6ccef30ff29c4a0
Joseph Santiago <joseph.santiago@Service-Unit-MacBook-Pro.local>
2017-03-27 04:30:10 +0800

    call templates

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,8 @@ def post_detail(request):
     return HttpResponse("<h1>Detail Here</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>List Here</h1>")
+    return render(request, 'index.html', {})
+    #return HttpResponse("<h1>List Here</h1>")
 
 def post_update(request):
     return HttpResponse("<h1>Update Here</h1>")
