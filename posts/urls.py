edd78b659794123262642bee1f1b4a8aad717202
Joseph Santiago <joseph.santiago@Service-Unit-MacBook-Pro.local>
2017-03-27 06:51:21 +0800

    get item or 404 query

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from posts.views import post_create, post_detail, post_list, post_update, post_d
 urlpatterns = [
     url(r'^list/$', post_list, name='post_list'),
     url(r'^create/$', post_create, name='post_create'),
-    url(r'^detail/$', post_detail, name='post_detail'),
+    url(r'^detail/(?P<id>\d+)/$', post_detail, name='post_detail'),
     url(r'^update/$', post_update, name='post_update'),
     url(r'^delete/$', post_delete, name='post_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create Here</h1>")
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail Here</h1>")
+def post_detail(request, id=None):
+    instance =get_object_or_404(Post, id=id)
+    # instance = Post.objects.get(id=3)
+    # return HttpResponse("<h1>Detail Here</h1>")
+    context = {
+        "title":"Details",
+        "instance":instance
+    }
 
+    return render(request, 'post_detail.html', context)
 def post_list(request):
     queryset = Post.objects.all()
     context = {
